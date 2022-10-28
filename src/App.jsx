0a6a835d25b21475d84e1444e8313add16e51bdf
NettoJM <nettojm86@gmail.com>
2022-10-28 20:35:01 -0300

    created constructor with state, created onInputChange method to handle the state, started using the state and the handler on Form and Card components

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,26 @@ import Card from './components/Card';
 import Form from './components/Form';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: '',
+    };
+  }
+
+  onInputChange = ({ target: { name, value } }) => {
+    this.setState({
+      [name]: value,
+    });
+  };
+
   render() {
+    const { name } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form />
-        <Card />
+        <Form cardName={ name } onInputChange={ this.onInputChange } />
+        <Card cardName={ name } />
       </div>
     );
   }
