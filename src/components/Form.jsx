8998e6f655fee95a5c3649fbd59a0eed8359c3b2
NettoJM <nettojm86@gmail.com>
2022-10-28 16:35:10 -0300

    added textarea to component

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,6 +1,14 @@
 import React, { Component } from 'react';
 import Input from './Input';
 
+/* um campo do tipo number que contenha o atributo data-testid="attr1-input". Este campo será usado para inserir o primeiro atributo da carta. Ele é livre para você adicionar o atributo que mais combinar com o seu baralho.
+
+um campo do tipo number que contenha o atributo data-testid="attr2-input". Este campo será usado para inserir o segundo atributo da carta. Ele é livre para você adicionar o atributo que mais combinar com o seu baralho.
+
+um campo do tipo number que contenha o atributo data-testid="attr3-input". Este campo será usado para inserir o terceiro atributo da carta. Ele é livre para você adicionar o atributo que mais combinar com o seu baralho.
+
+um campo do tipo text que contenha o atributo data-testid="image-input". Este campo será usado para inserir o caminho para imagem da carta. */
+
 class Form extends Component {
   render() {
     return (
@@ -11,6 +19,16 @@ class Form extends Component {
           type="text"
           testid="name-input"
         />
+        <label htmlFor="description">
+          <span>Descrição</span>
+          <textarea
+            id="description"
+            name="description"
+            rows="5"
+            cols="33"
+            data-testid="description-input"
+          />
+        </label>
       </form>
     );
   }
