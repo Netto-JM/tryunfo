218d797dab13014270fce074eedef6162379a74a
NettoJM <nettojm86@gmail.com>
2022-10-28 20:44:29 -0300

    used more state attributes as props

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,10 @@ class App extends React.Component {
     super();
     this.state = {
       name: '',
+      description: '',
+      attr1: '',
+      attr2: '',
+      attr3: '',
     };
   }
 
@@ -17,12 +21,25 @@ class App extends React.Component {
   };
 
   render() {
-    const { name } = this.state;
+    const { name, description, attr1, attr2, attr3 } = this.state;
     return (
       <div>
         <h1>Tryunfo</h1>
-        <Form cardName={ name } onInputChange={ this.onInputChange } />
-        <Card cardName={ name } />
+        <Form
+          cardName={ name }
+          onInputChange={ this.onInputChange }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+        />
+        <Card
+          cardName={ name }
+          cardDescription={ description }
+          cardAttr1={ attr1 }
+          cardAttr2={ attr2 }
+          cardAttr3={ attr3 }
+        />
       </div>
     );
   }
