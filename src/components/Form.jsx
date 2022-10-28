2a3fbf47c3fefcaaa644773a9428d9a621995c3e
NettoJM <nettojm86@gmail.com>
2022-10-28 16:14:41 -0300

    using Input component to render form inputs

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
+import Input from './Input';
 
 class Form extends Component {
   render() {
     return (
       <form>
-        <input type="text" data-testid="name-input" />
+        <Input
+          labelFor="name"
+          labelText="Nome"
+          type="text"
+          testid="name-input"
+        />
       </form>
     );
   }
