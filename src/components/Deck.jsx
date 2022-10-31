043ffdcb4c63d5b40652dcc2c18e341aa35ca8fb
NettoJM <nettojm86@gmail.com>
2022-10-31 00:42:19 -0300

    added button and a click handler to handle a card deletion

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -4,20 +4,28 @@ import Card from './Card';
 
 class Deck extends Component {
   render() {
-    const { deck } = this.props;
+    const { deck, onDeleteButtonClick } = this.props;
     return (
       <div>
         {deck.map((card, index) => (
-          <Card
-            key={ card.name + index }
-            cardName={ card.name }
-            cardDescription={ card.description }
-            cardAttr1={ card.attr1 }
-            cardAttr2={ card.attr2 }
-            cardAttr3={ card.attr3 }
-            cardRare={ card.rarity }
-            cardTrunfo={ card.trunfo }
-          />
+          <div key={ card.name + index }>
+            <Card
+              cardName={ card.name }
+              cardDescription={ card.description }
+              cardAttr1={ card.attr1 }
+              cardAttr2={ card.attr2 }
+              cardAttr3={ card.attr3 }
+              cardRare={ card.rarity }
+              cardTrunfo={ card.trunfo }
+            />
+            <button
+              type="button"
+              data-testid="delete-button"
+              onClick={ () => onDeleteButtonClick(card) }
+            >
+              Excluir
+            </button>
+          </div>
         ))}
       </div>
     );
@@ -37,6 +45,7 @@ Deck.propTypes = {
       cardTrunfo: PropTypes.bool.isRequired,
     }),
   ).isRequired,
+  onDeleteButtonClick: PropTypes.func.isRequired,
 };
 
 export default Deck;
